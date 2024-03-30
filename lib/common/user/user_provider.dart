8eb993f5b0f49e93d3ff5b2b2054b84c00666b41
charmeee <mandugohigh@gmail.com>
2024-03-31 01:16:07 +0900

    fix: 토큰 없을 시 고려해서 타입 nullabe하게 변경

--- lib/common/user/user_provider.dart
+++ lib/common/user/user_provider.dart
@@ -7,20 +7,20 @@ part 'user_provider.g.dart';
 @riverpod
 class AuthController extends _$AuthController {
   @override
-  Future<User?>  build() async{
-    User user = await _loginWithToken();
+  Future<User?> build() async {
+    User? user = await _loginWithToken();
     return user;
   }
 
   _loginWithToken() async {
-    await Future.delayed(Duration(seconds: 10));
+    // await Future.delayed(Duration(seconds: 10));
     return const User(id: '1', username: 'test');
+    //토큰이없거나 에러있으면 null을 반환
   }
 
   login() async {
     await Future.delayed(Duration(seconds: 10));
     state = const AsyncData(User(id: '1', username: 'test'));
-
   }
 
   logout() {
