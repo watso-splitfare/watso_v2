f4b64c69727e8bdd3c163f392566a9c03fdbff03
charmeee <mandugohigh@gmail.com>
2024-04-08 14:51:12 +0900

    feat: 자동로그인 로직 구성

--- lib/common/dio/dio_handling.dart
+++ lib/common/dio/dio_handling.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../storage/storage_type.dart';
 
 class CustomInterceptors extends InterceptorsWrapper {
   final FlutterSecureStorage storage;
@@ -16,29 +20,14 @@ class CustomInterceptors extends InterceptorsWrapper {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) {
-    print('REQUEST[${options.method}] => PATH: ${options.path}');
-    // final accessToken = storage.read(key: ACCESS_TOKEN_KEY);
-    // final accessToken = ref.read(secureStorageProvider).read(key: ACCESS_TOKEN_KEY);
-    //
-    // if (accessToken != null) {
-    //   options.headers['Authorization'] = 'Bearer $accessToken';
-    // }
+    log('[REQ] [${options.method}] ${options.uri} ${options.data} ${options.headers}');
+    final accessToken = storage.read(key: AuthToken.accessToken.name);
+
+    if (accessToken != null) {
+      options.headers['Authorization'] = 'Bearer $accessToken';
+    }
 
     return super.onRequest(options, handler);
-  }
-
-  @override
-  void onError(
-    err,
-    ErrorInterceptorHandler handler,
-  ) {
-    print(
-        'ERROR[${err.response?.statusCode}] => PATH: ${err.requestOptions.path}');
-    // if (err.response?.statusCode == 401) {
-    //   ref.read(userMeProvider.notifier).logout();
-    // }
-
-    return super.onError(err, handler);
   }
 
   @override
@@ -46,13 +35,42 @@ class CustomInterceptors extends InterceptorsWrapper {
     Response response,
     ResponseInterceptorHandler handler,
   ) {
-    print(
-        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
-
-    // if (response.statusCode == 401) {
-    //   ref.read(userMeProvider.notifier).logout();
-    // }
-
+    log('[RES] [${response.requestOptions.method}] ${response.requestOptions.uri} ${response.data?.toString()}  ');
     return super.onResponse(response, handler);
+  }
+
+  @override
+  Future<void> onError(
+    err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    log('[ERR] [${err.requestOptions.method}] ${err.requestOptions.uri} ${err.response?.statusCode} ${err.requestOptions.data} ${err.response?.data}');
+
+    if (err.response?.statusCode == 401) {
+      try {
+        final refreshToken =
+            await storage.read(key: AuthToken.refreshToken.name);
+        await storage.delete(key: AuthToken.accessToken.name);
+
+        // no refresh token
+        if (refreshToken == null) {
+          // 에러를 던질때는 handler.reject를 사용한다.
+          return handler.reject(err);
+        }
+        // refresh token이 있을 경우, 새로운 access token을 요청한다.
+        // 요청이 안되면 에러를 던진다.
+
+        return handler.reject(err);
+      } catch (e) {
+        try {
+          await storage.deleteAll();
+        } catch (e) {
+          log('Failed to delete all storage');
+        }
+        return handler.reject(err);
+      }
+    }
+
+    return super.onError(err, handler);
   }
 }
