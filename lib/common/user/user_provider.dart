20512bb31248828a8b70b4be4efbf9657b792f57
charmeee <mandugohigh@gmail.com>
2024-04-08 14:52:43 +0900

    feat: 카카오 로그인 api 추가

--- lib/common/user/user_provider.dart
+++ lib/common/user/user_provider.dart
@@ -1,5 +1,10 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../dio/dio.dart';
 import './user_model.dart';
 
 part 'user_provider.g.dart';
@@ -7,20 +12,27 @@ part 'user_provider.g.dart';
 @riverpod
 class AuthController extends _$AuthController {
   @override
-  Future<User?> build() async {
-    User? user = await _loginWithToken();
+  Future<MyUser?> build() async {
+    MyUser? user = await _loginWithToken();
     return user;
   }
 
   _loginWithToken() async {
     // await Future.delayed(Duration(seconds: 10));
-    return const User(id: '1', username: 'test');
+    // return const MyUser(id: '1', username: 'test');
+    return null;
     //토큰이없거나 에러있으면 null을 반환
   }
 
-  login() async {
-    await Future.delayed(Duration(seconds: 10));
-    state = const AsyncData(User(id: '1', username: 'test'));
+  loginWithKakao() async {
+    OAuthToken token = await UserApi.instance.loginWithKakaoTalk();
+    log('카카오톡으로 로그인 성공 accessToken: ${token.accessToken} , idToken: ${token.idToken}');
+    Dio dio = ref.watch(dioProvider);
+    var data = await dio.get('/auth/login/kakao', queryParameters: {
+      'access_token': token.accessToken,
+    });
+    log('카카오톡으로 로그인 성공 data: $data');
+    state = const AsyncData(MyUser(id: '1', username: 'test'));
   }
 
   logout() {
